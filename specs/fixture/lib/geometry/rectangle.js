1d3f5ff279467d88620f647e0924a8d27398e917
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-04-01 22:29:09 -0700

    Added external imports in specs

--- specs/fixture/lib/geometry/rectangle.js
+++ specs/fixture/lib/geometry/rectangle.js
@@ -1,3 +1,4 @@
+import { yellow } from 'chalk';
 import { log } from '../util/console';
 import Polygon from './polygon';
 
@@ -9,7 +10,7 @@ export default class Rectangle extends Polygon {
   // Here, sayName() is a subclassed method which
   // overrides their superclass method of the same name.
   sayName() {
-    log(`Sup! My name is ${this.name}.`);
+    log(`Sup! My name is ${yellow(this.name)}.`);
     super.sayHistory();
   }
 }

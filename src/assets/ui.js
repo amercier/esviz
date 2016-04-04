439e6c26e03f03b95349a8ea48d25706e685a4d4
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-04-03 17:09:04 -0700

    Added download feature

--- src/assets/ui.js
+++ src/assets/ui.js
@@ -10,4 +10,20 @@ $(document).ready(function() {
     console.log('mode', mode);
     updateMode(true);
   });
+
+  $('#control-download').on('click', function() {
+    var viz = $('#viz');
+    var width = viz.width();
+    var height = viz.height();
+    var viewBox = '0 0 ' + width  + ' + ' + height;
+    var data = [
+      '<?xml version="1.0"?>',
+      '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
+      '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="' + width + '" height="' + height + '" viewBox="' + viewBox + '" class="mode-' + mode + '">',
+      '<style>' + $('#styles').html() + '</style>',
+      viz.html(),
+      '</svg>'
+    ].join('\n');
+    download(data, 'esviz.svg', 'image/svg+xml');
+  });
 });
